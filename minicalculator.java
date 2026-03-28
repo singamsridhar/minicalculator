3423a4620ead8ec523529521682da8acb2716d7c
singamsridhar <singamsridhar98@gmail.com>
2026-03-28 15:20:43 +0530

    Add files via upload
    # 🎌 Anime Club – HTML & CSS Project
    
    ## 📌 Project Description
    
    This project is a simple animated website developed using HTML and CSS.
    It allows users to explore anime content in a visually attractive interface.
    
    ---
    
    ## 🚀 Features
    
    * Animated homepage
    * Stylish UI design
    * Anime cards layout
    * Navigation between pages
    
    ---
    
    ## 🖼️ Screenshots
    
    ### 🏠 Home Page
    
    <p align="center">
      <img src="img1.png" width="600">
    </p>
    
    ### 🎬 Anime Page
    
    <p align="center">
      <img src="img2.png" width="300">
      <img src="img3.png" width="300">
      <img src="img4.png" width="300">
    </p>
    
    <p align="center">
      <img src="img5.png" width="300">
      <img src="img6.png" width="300">
      <img src="img7.png" width="300">
    </p>
    
    ---
    
    ## 🛠️ Technologies Used
    
    * HTML
    * CSS
    
    ---
    
    ## 🌐 Live Website
    
    https://singamsridhar.github.io/anime-club/
    
    ---
    
    ## 🔗 GitHub Repository
    
    https://github.com/singamsridhar/anime-club
    
    ---
    
    ## 👨‍💻 Author
    
    Sridhar
    CSE Student
    NC.SC.U4CSE25048

---
+++ minicalculator.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class minicalculator extends JFrame implements ActionListener {
+
+    JTextField t1, t2, result;
+    JButton add, sub, mul, div, clear, exit;
+
+    minicalculator() {
+        setTitle("Advanced Calculator 😎");
+        setSize(350, 300);
+        setLayout(new FlowLayout());
+
+        t1 = new JTextField(10);
+        t2 = new JTextField(10);
+        result = new JTextField(20);
+        result.setEditable(false);
+
+        add = new JButton("+");
+        sub = new JButton("-");
+        mul = new JButton("*");
+        div = new JButton("/");
+        clear = new JButton("Clear");
+        exit = new JButton("Exit");
+
+        add(new JLabel("Enter Number 1:"));
+        add(t1);
+        add(new JLabel("Enter Number 2:"));
+        add(t2);
+
+        add(add);
+        add(sub);
+        add(mul);
+        add(div);
+
+        add(clear);
+        add(exit);
+
+        add(new JLabel("Result:"));
+        add(result);
+
+        // Action listeners
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+        clear.addActionListener(this);
+        exit.addActionListener(this);
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            if (e.getSource() == clear) {
+                t1.setText("");
+                t2.setText("");
+                result.setText("");
+                return;
+            }
+
+            if (e.getSource() == exit) {
+                System.exit(0);
+            }
+
+            double a = Double.parseDouble(t1.getText());
+            double b = Double.parseDouble(t2.getText());
+            double res = 0;
+
+            if (e.getSource() == add)
+                res = a + b;
+            else if (e.getSource() == sub)
+                res = a - b;
+            else if (e.getSource() == mul)
+                res = a * b;
+            else if (e.getSource() == div) {
+                if (b == 0) {
+                    result.setText("Cannot divide by zero ❌");
+                    return;
+                }
+                res = a / b;
+            }
+
+            result.setText("Result: " + res);
+
+        } catch (NumberFormatException ex) {
+            result.setText("Invalid Input ❌");
+        }
+    }
+
+    public static void main(String[] args) {
+        new AdvancedCalculator();
+    }
+}
